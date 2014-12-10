92e0d5106a69d9af304609bb0f93eef13a9db319
qmatteoq <info@qmatteoq.com>
2014-12-10 22:41:04 +0100

    added first basic demo

---
+++ Prism-BasicDemo/PrismDemo.Shared/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Practices.Prism.Mvvm;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Microsoft.Practices.Prism.Mvvm.Interfaces;
+using Microsoft.Practices.Unity;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace PrismDemo
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : MvvmAppBase
+    {
+        IUnityContainer _container = new UnityContainer();
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Main", null);
+            return Task.FromResult<object>(null);
+        }
+
+        protected override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            // Register MvvmAppBase services with the container so that view models can take dependencies on them
+            _container.RegisterInstance<ISessionStateService>(SessionStateService);
+            _container.RegisterInstance<INavigationService>(NavigationService);
+            // Register any app specific types with the container
+
+            // Set a factory for the ViewModelLocator to use the container to construct view models so their 
+            // dependencies get injected by the container
+            ViewModelLocationProvider.SetDefaultViewModelFactory((viewModelType) => _container.Resolve(viewModelType));
+            return Task.FromResult<object>(null);
+        }
+
+    }
+}
---
+++ Prism-BasicDemo/PrismDemo.Shared/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+using Microsoft.Practices.Prism.Mvvm.Interfaces;
+
+namespace PrismDemo.ViewModels
+{
+    public class MainPageViewModel : ViewModel
+    {
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            SayHelloCommand = new DelegateCommand(() =>
+            {
+                Message = string.Format("Hello {0}", Name);
+            }, () => !string.IsNullOrEmpty(Name));
+        }
+
+        public DelegateCommand SayHelloCommand { get; private set; }
+
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                SayHelloCommand.RaiseCanExecuteChanged();
+                SetProperty(ref _name, value);
+            }
+        }
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+    }
+}
